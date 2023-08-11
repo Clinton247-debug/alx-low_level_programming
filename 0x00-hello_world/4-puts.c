197d7d2e70e85bd2b49be9464a926fe2bf3d128b
Clinton247-debug <depopnwinson@gmail.com>
2023-08-10 19:00:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 /**
  * main - A program that prints a line with puts fuction
- *
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle,");
+return (0);
 }
