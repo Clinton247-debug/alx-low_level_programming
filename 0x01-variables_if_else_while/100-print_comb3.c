aa2132d02d7806283eb656aad5b947fdf66be65d
Clinton247-debug <depopnwinson@gmail.com>
2023-08-11 21:40:08 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,30 +8,28 @@
 int main(void)
 {
 	int i = '0';
-	int j = '0';
+	int j = '1';
 
-	while (i <= '9')
+	while (i <= '8')
 	{
 		while (j <= '9')
 		{
-			if (!(i > j) || i == j)
+			putchar(i);
+			putchar(j);
+
+			if (i == '8' && j == '9')
 			{
-				putchar(i);
-				putchar(j);
-				if (i == '8' && j == '9')
-				{
-					putchar('\n');
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
 			}
 			j++;
 		}
+		i++;
+		j = i + 1;
 	}
-	j = '0';
-	i++;
 	return (0);
 }
