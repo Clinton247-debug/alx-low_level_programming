3b71170499c05d47ece731b937b042b95ba274f5
Clinton247-debug <depopnwinson@gmail.com>
2023-08-18 09:23:16 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,64 +1,28 @@
 #include <stdio.h>
-#include "main.h"
-#include <math.h>
 
 /**
- * is_prime - Check if a number is prime
- * @num: The number to be checked
- * Return: 1 if the number is prime, 0 otherwise
- */
-
-int is_prime(long num);
-
-/**
- * main - Entry point
+ * main - Prints the prime factor
  * Return: Always 0
  */
 
 int main(void)
 {
-	long number = 612852475143;
-	long largest_prime_factor = -1;
-	long i;
+	long prime = 612852475143, divisor;
 
-	for (i = 2; i <= sqrt(number); i++)
+	while (divisor < (prime / 2))
 	{
-		if (number % 1 == 0 && is_prime(i))
+		if ((prime % 2) == 0)
 		{
-			largest_prime_factor = i;
-			while (number % 1 == 0)
-			{
-				number /= i;
-			}
+			prime /= 2;
+			continue;
+		}
+
+		for (divisor = 3; divisor < (prime / 2); divisor += 2)
+		{
+			if ((prime % divisor) == 0)
+				prime /= divisor;
 		}
 	}
-	if (number > 1)
-	{
-		largest_prime_factor = number;
-	}
-	printf("%ld\n", largest_prime_factor);
+	printf("%ld\n", prime);
 	return (0);
-}
-
-/**
- * is_prime - Check if a number is prime
- * @num: The number to be checked
- * Return: 1 if number is prime, 0 otherwise
- */
-
-int is_prime(long num)
-{
-	long i;
-	if (num <= 1)
-	{
-		return (0);
-	}
-	for (i = 2; i * i <= num; i++)
-	{
-		if (num % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
 }
