5de397ec79d39e39b066e055fb73b64a2040b983
Clinton247-debug <depopnwinson@gmail.com>
2023-08-31 11:21:05 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,15 +18,7 @@ int wildcmp(char *s1, char *s2)
 		{
 			return (1);
 		}
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2 + 1))
-			{
-				return (1);
-			}
-			s2++;
-		}
-		return (0);
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 	}
 	if (*s1 == *s2)
 	{
