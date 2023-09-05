edd7152da8fde33f3553f5df5eb3e5f6b9c7a6b1
Clinton247-debug <depopnwinson@gmail.com>
2023-09-05 01:21:15 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguements
+ * @argv: An array containingthe command-line arguements
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 
+int _putchar(char c);
 int main(int argc, char *argv[]);
 
 #endif
