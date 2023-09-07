a1041b8959caff836b8be60e8368332bce3b3af3
Clinton247-debug <depopnwinson@gmail.com>
2023-09-07 15:55:37 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,6 +29,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	min_size = (old_size < new_size) ? old_size : new_size;
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
 	new_ptr = malloc(new_size);
 
 	if (new_ptr == NULL)
