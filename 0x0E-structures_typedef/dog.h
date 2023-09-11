5053ad1ab8aede309cf21dcf7ef8a02412007a1d
Clinton247-debug <depopnwinson@gmail.com>
2023-09-11 04:32:00 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdlib.h>
+
+/**
+ * struct dog - Defines a structure to represent a dog
+ * @name: Pointer the dog's name
+ * @age: The dog's age
+ * @owner: Name of the dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
