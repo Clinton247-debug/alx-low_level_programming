ce361679c7471cfe1a78048e827d42239ab54b28
Clinton247-debug <depopnwinson@gmail.com>
2023-09-13 07:58:00 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the stdout
+ * @c: The character to be written
+ * Return: On success, returns the written character as unsigned cha cast
+ * On error, -1 is returned, errno is set appropriately
+ */
+
+int putchar(char c)
+{
+	return (write(1, &c, 1));
+}
