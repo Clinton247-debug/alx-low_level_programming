f76b7869e3b92eded00e7526dc7c231d50bf9827
Clinton247-debug <depopnwinson@gmail.com>
2023-09-13 07:44:07 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "function_pointers.h"
+#include <unistd.h>
+
 
 /**
  * print_name - Function that prints a name
@@ -12,5 +14,6 @@ void print_name(char *name, void (*f)(char *))
 	if (name != NULL || f != NULL)
 	{
 		f(name);
+		write(1, "\n", 1);
 	}
 }
