a8387d8181af6f058ed557ecfab7c272dbc461c5
Clinton247-debug <depopnwinson@gmail.com>
2023-09-14 13:10:50 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,22 +11,22 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b, result;
+	int num1, num2, result;
 	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
 		exit(98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	operation = get_op_func(argv[2]);
 	
 	if (operation == NULL)
 	{
 		exit(99);
 	}
-	result = operation(a, b);
+	result = operation(num1, num2);
 	printf("%d\n", result);
 	return (0);
 }
