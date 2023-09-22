53da51f91a0559908523a80f5178c5fd01071385
Clinton247-debug <depopnwinson@gmail.com>
2023-09-22 07:02:02 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void before_main(void) __attribute__((constructor));
+
+
+/**
+ * before_main - Function to be executed before main
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
