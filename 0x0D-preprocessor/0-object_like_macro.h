63472bd81de617214ad392c2f611502d009e47f2
Clinton247-debug <depopnwinson@gmail.com>
2023-10-07 12:12:34 +0100

    Deleted Files

--- 0x0D-preprocessor/0-object_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef MYHEADER_H
-#define MYHEADER_H
-
-#define SIZE 1024
-
-#endif
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-
-#define SUM(x, y) ((x) + (y))
-
-#endif
